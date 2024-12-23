d7a982940c2485092a668f146e8dbaed653ed219
James Carl Llamedo <llamedojamescarl3221@gmail.com>
2024-12-23 18:39:31 +0800

    Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
           className="w-[28px] h-[28px] object-contain cursor-pointer"
           onClick={() => setToggle(!toggle)}
            />
-
+ 
            <div className={`${!toggle ? 'hidden':'flex' } p-6 black-gradient absolute 
            top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
 
@@ -81,5 +81,4 @@ const Navbar = () => {
 
 export default Navbar 
 
-
-35:35
+// 35:35
