6e76f8b4b2a56aa563aeff10ac2a014831bedcbf
James Carl Llamedo <llamedojamescarl3221@gmail.com>
2024-12-24 15:54:28 +0800

    12/24/25

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -24,9 +24,10 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">James <span
-          className="sm:block hidden">| Portfolio</span></p>
-
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            James &nbsp; 
+            <span className="sm:block hidden">| Portfolio</span>
+            </p>
         </Link>
 
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -64,8 +65,11 @@ const Navbar = () => {
                 active === Link.title
                   ? "text-white"
                   : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(Link.title)}
+              } font-poppins font-medium cursor-pointer text-[16px]`}
+              onClick={() => {
+                setToggle(!toggle);
+                setActive(Link.title);
+              }}
             >
               <a href={`#${Link.id}`}>{Link.title}</a>
             </li>
